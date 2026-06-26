27e4c4ba4ceb46c3e7e6b5e3854e28120ebc3ae1
swedishsplidney <kipnhackett@gmail.com>
2026-06-25 22:39:28 -0600

    workflow fix

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qml/main.qml")));
+    engine.load(QUrl(QStringLiteral("qrc:/fraktalized/qml/main.qml")));
 
     return app.exec();
 }
